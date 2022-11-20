ba519f70279590434d413df81a74b58a20c42dd8
choisangwon <rngus3698@naver.com>
2022-11-20 18:18:27 +0900

    테이블 주문내역 조회 api

--- controllers/spacecontroller.js
+++ controllers/spacecontroller.js
@@ -10,3 +10,15 @@ exports.spacelist = async (req, res) => {
 
     return res.status(200).json({res_code: "0000", message: "테이블 정보 조회 성공", spacelist: spaceserviceres.spacequeryset});
 }
+
+exports.orderlist = async (req, res) => {
+
+    let spacepkey = req.query.spacepkey;
+
+    let spaceserviceres = await spaceservice.orderlist(spacepkey);
+    if (spaceserviceres.retcode == "-99") {
+        return res.status(500).json({res_code: "9999", message: "데이터베이스 오류"})
+    }
+
+    return res.status(200).json({res_code: "0000", message: "테이블 주문정보 조회 성공", space: spaceserviceres.space, orderlist: spaceserviceres.orderlist})
+}
--- services/spaceservice.js
+++ services/spaceservice.js
@@ -28,3 +28,44 @@ exports.spacelist = async () => {
 
     return {retcode: "00", spacequeryset}
 }
+
+exports.orderlist = async (spacepkey) => {
+    // TODO: 주문내역 작업 필요함
+
+    let sql = `
+        select 
+            sp.spacepkey, spacenum, oi.orderinfopkey, totalpayprice,
+            om.menupkey, menuname, saleprice, count
+        from space sp
+        left join orderinfo oi on sp.spacepkey=oi.spacepkey
+        join ordermenu om on oi.orderinfopkey=om.orderinfopkey
+        where sp.spacepkey=? and oi.payyn='N'
+    `;
+
+    const connection = await db.getConnection();
+    let spaceorderqueryset;
+    try{
+        spaceorderqueryset = await new Promise((resolve, reject) => {
+            connection.query(sql, [spacepkey], (err, rows) => {
+                if(err) reject(err);
+                else resolve(rows.length > 0? rows: null);
+            })
+        })
+    }catch (err) {
+        console.log(err);
+        connection.release();
+        return {retcode: "-99", message: err.toString()}
+    }
+
+    connection.release();
+
+    let space = {
+        spacepkey: spaceorderqueryset[0].spacepkey,
+        spacenum: spaceorderqueryset[0].spacenum,
+        orderinfopkey: spaceorderqueryset[0].orderinfopkey,
+        totalpayprice: spaceorderqueryset[0].totalpayprice
+    }
+
+    return {retcode: "00", space, orderlist: []}
+
+}
