5edda30982599ebecbed0fffe9d8582aead5c701
choisangwon <rngus3698@naver.com>
2022-11-25 13:18:24 +0900

    aes 암호화 복호화 함수 수정

--- common/encrypto.js
+++ common/encrypto.js
@@ -3,13 +3,11 @@ const crypto = require('crypto');
 const key = '0000000000@asdasdas#adadadskey00';
 const iv = '0000000000000000';
 // 암호화 AES256
-exports.encrypt = (data) => {
-    return new Promise(async (resolve) => {
-        const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-        let encrypt = cipher.update(data, 'utf8', 'base64');
-        encrypt += cipher.final('base64');
-        resolve(encrypt)
-    })
+exports.encrypt = async (data) => {
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let encrypt = cipher.update(data, 'utf8', 'base64');
+    encrypt += cipher.final('base64');
+    return encrypt
 }
 
 // 복호화 AES256
@@ -18,10 +16,4 @@ exports.decrypt = async (data) => {
     let decrypt = decipher.update(data, 'base64', 'utf8');
     decrypt += decipher.final('utf8');
     return decrypt
-    // return new Promise(async (resolve) => {
-    //     const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-    //     let decrypt = decipher.update(data, 'base64', 'utf8');
-    //     decrypt += decipher.final('utf8');
-    //     resolve(decrypt)
-    // })
 }
