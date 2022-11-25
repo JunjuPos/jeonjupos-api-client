00426e05fc55b01023223186c0b3036bb786d499
choisangwon <rngus3698@naver.com>
2022-11-25 18:08:36 +0900

    테이블 리스트 조회 총가격, 주문메뉴 리스트 추가

--- services/spaceservice.js
+++ services/spaceservice.js
@@ -4,10 +4,52 @@ exports.spacelist = async () => {
     const connection = await db.getConnection();
 
     const getSpaceListQuery = `select spacepkey, spacenum, cookingyn from space where isactiveyn='Y'`;
+    const getSpaceOrderQuery = `
+        select sp.spacepkey, om.menuname, om.saleprice, om.count
+        from space sp 
+        join orderinfo oi on sp.spacepkey=oi.spacepkey
+        join ordermenu om on oi.orderinfopkey=om.orderinfopkey
+        where oi.spacepkey in ? and sp.cookingyn='Y' and oi.payyn='N'
+    `;
 
     return new Promise(async (resolve) => {
+        // 테이블 리스트 조회
         connection.query(getSpaceListQuery, [], (err, rows) => {
-            resolve(err ? {retcode: "-99", message: err.toString()} : {retcode: "00", spacequeryset: rows})
+            if (err) {
+                resolve({retcode: "-99", message: err.toString()})
+            }
+
+            // 테이블 리스트
+            const spacelist = rows;
+
+            //  테이블 고유번호 리스트 생성
+            const spacepkeylist = rows.map(space => space.spacepkey);
+
+            // 테이블별 주문메뉴 리스트 조회
+            connection.query(getSpaceOrderQuery, [[spacepkeylist]], (err, rows) => {
+                if (err) {
+                    resolve({retcode: "-99", message: err.toString()})
+                }
+
+                // 테스트 리스트에 주문한 메뉴리스트 추가
+                // rows : 테이블 주문내역
+                for (let space of spacelist) {
+                    space.amount = 0        // 테이블당 총 주문금액
+                    space.orderlist = []    // 테이블당 주문내역
+                    rows.find((row) => {
+                        if (space.spacepkey === row.spacepkey) {
+                            space.amount = row.count * row.saleprice
+                            space.orderlist.push({
+                                menuname: row.menuname,
+                                menucount: row.count,
+                                saleprice: row.saleprice
+                            })
+                        }
+                    })
+                }
+
+                resolve(spacelist);
+            })
         })
 
         connection.release();
