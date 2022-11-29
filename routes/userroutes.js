9977b4aeb2bf8f52d68e7cf3b09c757a3b34de31
choisangwon <rngus3698@naver.com>
2022-11-29 15:42:32 +0900

    후불명단 검색 api 추가

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -2,6 +2,6 @@ let express = require('express');
 let router = express.Router();
 let usercontroller = require("../controllers/usercontroller");
 
-router.get("/postpaidgroup/list", usercontroller.postpaidgrouplist);
+router.get("/postpaidgroup/list", usercontroller.postpaidgrouplist); //
 
 module.exports = router;
